6f042e270f71406dcd1faca08f0fcad79af148c5
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-06-20 18:18:43 -0400

    Add files via upload

---
+++ Handler/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ares.Handler
+{
+    internal class Data
+    {
+        public static bool procOpen;
+        
+        //ammo pointers
+        public static string deagleAmmoPointer = "base+0148485C,2DC";
+        public static string interventionAmmoPointer = "base+0148485C,2EC";
+    }
+}
