cdfaf7ca5b27219b8c3ec22c8882c80832e8eb47
romance <66000596+romance999@users.noreply.github.com>
2022-06-29 13:23:50 -0400

    Add M40A3 ammo pointer

--- Handler/Data.cs
+++ Handler/Data.cs
@@ -13,5 +13,6 @@ namespace AresGUI.Handler
         //ammo pointers
         public static string deagleAmmoPointer = "base+0148485C,2DC";
         public static string interventionAmmoPointer = "base+0148485C,2EC";
+        public static string m40a3AmmoPointer = "base+0148485C,2EC";
     }
 }
